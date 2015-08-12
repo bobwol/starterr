1336b73410e1ad4d65e7a0b5ed0fe67cb7e46b16
Nico <n.pernice@theprintlabs.com>
2015-08-12 17:39:20 +0100

    fix bower dependecies

--- assets/js/source/_main__init.js
+++ assets/js/source/_main__init.js
@@ -35,10 +35,3 @@ if (n < 0) {
 } else {
 	document.documentElement.className += " fonts-loaded";
 }
-
-var elem = document.querySelector('.grid');
-var msnry = new Masonry( elem, {
-  // options
-  itemSelector: '.grid-item',
-  columnWidth: 200
-});
